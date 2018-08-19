31353528b0ddf9d778aeef04a778072c79f2d490
ekolis <ekolis@localhost>
2018-08-19 13:28:48 -0400

    Replaced say command with kill command

---
+++ ChancellorGerath/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChancellorGerath
+{
+	public static class Extensions
+	{
+		/// <summary>
+		/// Our friendly neighborhood random number generator.
+		/// </summary>
+		private static Random Random { get; } = new Random();
+
+		/// <summary>
+		/// Picks a random item from a list.
+		/// </summary>
+		/// <typeparam name="T">The type of item.</typeparam>
+		/// <param name="list">The list.</param>
+		/// <returns>A randomly chosen item from the list.</returns>
+		public static T PickRandom<T>(this IEnumerable<T> list)
+		{
+			return list.ElementAt(Random.Next(list.Count()));
+		}
+	}
+}
---
+++ ChancellorGerath/Verbs/VerbsModule.cs
@@ -0,0 +1,36 @@
+﻿using Discord.Commands;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChancellorGerath.Verbs
+{
+	/// <summary>
+	/// Allows the bot to say or do things in response to commands.
+	/// </summary>
+	public class VerbsModule : ModuleBase<SocketCommandContext>
+	{
+		// !kill Bob -> a random attack message directed against Bob, unless Bob is on the immortals list.
+		[Command("kill")]
+		[Summary("Attacks someone fatally.")]
+		public Task KillAsync([Remainder] [Summary("Who to kill")] string target)
+		{
+			foreach (var immortal in Immortals.Keys)
+			{
+				if (target.Contains(immortal))
+					return ReplyAsync(Immortals[immortal]);
+			}
+			return ReplyAsync($"*{Attacks.PickRandom()} {target} with {Weapons.PickRandom()}*");
+		}
+
+		private string[] Attacks = File.ReadAllLines("Verbs/Attacks.txt");
+
+		private string[] Weapons = File.ReadAllLines("Verbs/Weapons.txt");
+
+		private IDictionary<string, string> Immortals = JsonConvert.DeserializeObject<IDictionary<string, string>>(File.ReadAllText("Verbs/Immortals.json"));
+	}
+}
