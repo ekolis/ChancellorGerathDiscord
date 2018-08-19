008ec88c6b7ba8a74ecf2918141d666ae0ba3c9e
ekolis <ekolis@localhost>
2018-08-18 22:39:17 -0400

    Initial commit

---
+++ ChancellorGerath/Program.cs
@@ -0,0 +1,42 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ChancellorGerath
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		=> new Program().MainAsync().GetAwaiter().GetResult();
+
+		private DiscordSocketClient client;
+
+		public async Task MainAsync()
+		{
+			client = new DiscordSocketClient();
+
+			client.Log += Log;
+
+			// Remember to keep token private or to read it from an 
+			// external source! In this case, we are reading the token 
+			// from an environment variable. If you do not know how to set-up
+			// environment variables, you may find more information on the 
+			// Internet or by using other methods such as reading from 
+			// a configuration.
+			var token = File.ReadAllText("Token.txt");
+			await client.LoginAsync(TokenType.Bot, token);
+			await client.StartAsync();
+
+			// Block this task until the program is closed.
+			await Task.Delay(-1);
+		}
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+	}
+}
