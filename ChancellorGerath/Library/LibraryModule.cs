f1f5f9eab388607d0804650e805e4504e2eae2e0
Ed Kolis <edkolis@gmail.com>
2018-08-20 22:08:28 -0400

    Added help command which generates help text from reflection metadata.

--- ChancellorGerath/Library/LibraryModule.cs
+++ ChancellorGerath/Library/LibraryModule.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -11,7 +12,7 @@ using System.Web;
 namespace ChancellorGerath.Library
 {
 	/// <summary>
-	/// Allows the bot to say or do things in response to commands.
+	/// Allows the bot to provide useful or not-so-useful information.
 	/// </summary>
 	public class LibraryModule : MyModuleBase<SocketCommandContext>
 	{
@@ -26,8 +27,56 @@ namespace ChancellorGerath.Library
 				return ReplyAsync($"Sorry, I don't know anything about {topic}. Try Googling it? http://www.google.com/search?q={HttpUtility.UrlEncode(topic)}");
 		}
 
+		// !help -> gives a list of valid bot commands
+		[Command("help")]
+		[Summary("Gives a list of valid bot commands.")]
+		public Task HelpAsync()
+		{
+			return ReplyAsync("I know the following commands: " + string.Join(", ", CommandDefinitions.Select(c => c.Name).Distinct()));
+		}
+
+		// !help kill -> gives info about the kill command
+		[Command("help")]
+		[Summary("Gives instructions on how to use a particular command.")]
+		public Task HelpAsync([Remainder] [Summary("Command to get help on")] string cmd)
+		{
+			var defs = CommandDefinitions.Where(d => d.Name == cmd);
+			return ReplyAsync(string.Join("\n", defs.Select(d => d.Name + " " + string.Join(", ", d.Parameters.Keys) + ": " + d.Summary)));
+		}
+
+		/// <summary>
+		/// Loads the command definitions by reflecting on this assembly.
+		/// </summary>
+		/// <returns></returns>
+		private static IEnumerable<CommandDefinition> LoadCommandDefinitions()
+		{
+			foreach (var t in Assembly.GetExecutingAssembly().DefinedTypes)
+			{
+				foreach (var m in t.GetMethods())
+				{
+					var cmdatt = m.GetCustomAttribute<CommandAttribute>();
+					if (cmdatt != null)
+					{
+						var name = cmdatt.Text;
+						var summary = m.GetCustomAttribute<SummaryAttribute>()?.Text ?? "I don't know what that command does!";
+						var def = new CommandDefinition { Name = name, Summary = summary };
+						foreach (var parm in m.GetParameters())
+							def.Parameters.Add(parm.Name, parm.GetCustomAttribute<SummaryAttribute>()?.Text ?? "I don't know what this parameter does!");
+						yield return def;
+					}
+				}
+			}
+		}
+
+		private List<CommandDefinition> CommandDefinitions { get; } = LoadCommandDefinitions().ToList();
+
 		private IDictionary<string, string> Topics { get; } = JsonConvert.DeserializeObject<IDictionary<string, string>>(File.ReadAllText("Library/Topics.json"));
 
-
+		private class CommandDefinition
+		{
+			public string Name { get; set; }
+			public string Summary { get; set; }
+			public IDictionary<string, string> Parameters { get; private set; } = new Dictionary<string, string>();
+		}
 	}
 }
